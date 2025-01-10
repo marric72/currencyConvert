4ecceb1722a1603bcecdaeddfb9ff7f6cdc7f200
Heather <71154676+marric72@users.noreply.github.com>
2025-01-10 13:59:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ st.markdown(title, unsafe_allow_html=True)
 st.image("computer.png", caption=' ')
 
 
-instructions='<p style="font-family:Courier; color:Black; font-size: 20px;">Start on <b>prclab1</b> in the directory<br> <b>/home/faculty/marric72/cs125_CTF</b> <br>Use the commands we learned in class (ex. cd, cat, ls) to navigate and find the flags.  Enter your answers in the boxes to the right of the questions and then hit Check Answers.<br><br>Happy Hunting! (Hint: Flags look like: FLG-####)<br><br>Sometimes I do not want to give the whole file name, so I use stars in place of characters below.</p>'
+instructions='<p style="font-family:Courier; color:Black; font-size: 20px;">Start on <b>prclab1</b> in the directory<br> <b>/home/faculty/marric72/cs125_CTF</b> <br>Use the commands we learned in class (ex. cd, cat, ls) to navigate and find the flags.  Enter your answers in the boxes to the right of the questions and then hit Enter<br><br>Happy Hunting! (Hint: Flags look like: FLG-####)<br><br>Sometimes I do not want to give the whole file name, so I use stars in place of characters below.</p>'
 st.markdown(instructions, unsafe_allow_html=True)
 
 
