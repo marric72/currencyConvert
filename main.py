b90a413e402126eb722f1e175703dd73a55f69ac
Heather <71154676+marric72@users.noreply.github.com>
2025-01-10 10:24:12 -0700

    Update main.py
    update to create CS125 CTF

--- main.py
+++ main.py
@@ -12,30 +12,30 @@ st.set_page_config(
 
 
 #Write text to the screen with HTML markup
-title = '<p style="font-family:Courier; color:Blue; font-size: 30px;">CS399 Streamlit Test</p>'
+title = '<p style="font-family:Courier; color:Blue; font-size: 30px;">prclab1 Capture the Flag</p>'
 st.markdown(title, unsafe_allow_html=True)
-st.image("money.png", caption='Save your Money')
+st.image("computer.png", caption=' ')
 title = '<p style="font-family:Courier; color:White; font-size: 20px;">Exchange Rates </p>'
 st.markdown(title, unsafe_allow_html=True)
 
-st.write("Pulling data from: https://api.exchangerate-api.com/v4/latest/USD")
-url = 'https://api.exchangerate-api.com/v4/latest/USD'
-result=get(url, timeout=3).json()
+#st.write("Pulling data from: https://api.exchangerate-api.com/v4/latest/USD")
+#url = 'https://api.exchangerate-api.com/v4/latest/USD'
+#result=get(url, timeout=3).json()
 
 # Convert rates data to DataFrame
-df = pd.DataFrame(result.get("rates"), index=["Rates"])
+#df = pd.DataFrame(result.get("rates"), index=["Rates"])
 
 # Display multi-select for choosing columns
-selected_columns = st.multiselect(
-    'Select columns to display:',
-    list(df.columns),
-    default=['USD', 'EUR', 'GBP']  # Default columns to display
-)
+#selected_columns = st.multiselect(
+#    'Select columns to display:',
+#    list(df.columns),
+#    default=['USD', 'EUR', 'GBP']  # Default columns to display
+#)
 
 # Filter DataFrame based on selected columns
-filtered_df = df[selected_columns]
+#filtered_df = df[selected_columns]
 
 # Display the filtered DataFrame
-st.title("All the Data")
-st.write(filtered_df)
+#st.title("All the Data")
+#st.write(filtered_df)
 
