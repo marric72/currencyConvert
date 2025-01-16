4279f84e13ad627c0275c79513bedb522819dffe
Heather <71154676+marric72@users.noreply.github.com>
2025-01-16 12:16:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ title = '<p style="font-family:Courier; color:White; font-size: 30px;">prclab1 C
 st.markdown(title, unsafe_allow_html=True)
 st.image("computer.png", caption=' ')
 
-instructions = '<p style="font-family:Courier; color:Black; font-size: 20px;">Use Linux command line only. <br> Start on <b>prclab1</b> in the directory<br> <b>/home/faculty/marric72/cs125_CTF</b> <br>Use the commands we learned in class (ex. cd, cat, ls) to navigate and find the flags. Enter your answers in the boxes to the right of the questions and then hit Enter.<br><br>Happy Hunting! (Hint: Flags look like: FLG-###)<br><br>Sometimes I do not want to give the whole filename, so I use stars in place of characters below.</p>'
+instructions = '<p style="font-family:Courier; color:Blue; font-size: 20px;">Use Linux command line only. <br> Start on <b>prclab1</b> in the directory<br> <b>/home/faculty/marric72/cs125_CTF</b> <br>Use the commands we learned in class (ex. cd, cat, ls) to navigate and find the flags. Enter your answers in the boxes to the right of the questions and then hit Enter.<br><br>Happy Hunting! (Hint: Flags look like: FLG-###)<br><br>Sometimes I do not want to give the whole filename, so I use stars in place of characters below.</p>'
 st.markdown(instructions, unsafe_allow_html=True)
 
 # Create three columns: one for the question, one for the input box, and one for feedback
